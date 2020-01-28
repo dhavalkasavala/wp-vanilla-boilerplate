464dc8f84a961f681deafecaecff79b56f4fae6b
dhavalkasavala <dhaval.wpas@gmail.com>
2020-01-28 22:46:42 +0530

    Modify escape data functions

--- header.php
+++ header.php
@@ -31,7 +31,7 @@
 			  </div>
 			  <div class="block lg:hidden">
 				<button class="flex items-center px-3 py-2 border rounded text-teal-lighter border-teal-light hover:text-white hover:border-white menuToggle">
-				  <svg class="h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
+				  <svg class="h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title><?php _e('Menu','wp-vanilla'); ?></title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
 				</button>
 			  </div>
 			  <div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto" id="menu-mainmenu">
@@ -49,7 +49,7 @@
 
 						$output = strip_tags( wp_nav_menu( $menuParam ), '<a>' );
 						$output = preg_replace( '/<a/', '<a class="block mt-4 lg:inline-block lg:mt-0 text-teal-lighter hover:text-purple-600 mr-4"', $output );
-						echo html_entity_decode(esc_html( $output ));
+						echo wp_kses_post( $output );
 					}
 					?>
 				</div>
